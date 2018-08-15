5e32fe2c29252845b5e547b0cbbb211fe2f4eade
C.Rodriguez <crod@madebycrod.com>
2018-08-15 10:25:55 -0600

    add HW3

---
+++ HW3_Problem2.m
@@ -0,0 +1,32 @@
+% HW3
+% 
+%
+
+clear all;
+
+syms q
+
+% Total Revenue Function
+TR = 5900*q - 10*q^2;
+
+% Total Cost Function
+
+TC = 2*q^3 - 4*q^2 + 140*q + 845;
+
+% Profit Function
+
+profit = TR-TC;
+pretty(profit);
+disp(profit);
+crit_pts = solve(profit);
+
+disp(crit_pts);
+
+fplot(profit);
+
+% fplot(@(x) profit, xinterval [0 100],'b');
+hold on
+grid on;
+plot(double(crit_pts), double(subs(profit,crit_pts)),'ro');
+title('Maximum and Minimum of Profit');
+hold off
